60169133b203c8808319f5a575385886b084ca20
= <=>
2020-05-07 00:58:50 +0200

    edit consumer

--- backend/consumer/consumer.py
+++ backend/consumer/consumer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import pika
-from model import Matches, Title, Team, Tournament
+from model import Title, Team, Tournament # Matches
 import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -15,24 +15,24 @@ def callback(ch, method, properties, body):
     # add title
     print('add title')
     print(output_json["data"]["title"],output_json["data"]["date_start_text"])
-    Title.Insert(output_json["data"]["title"],output_json["data"]["date_start_text"],output_json["data"]["state"])
+    Title.Insert(output_json["data"]["title"],output_json["data"]["state"])
 
+    # add tournamte
+    print('add tournamte',output_json["data"]["title"])
+    tmp = output_json["data"]["tournament"]
+    if len(tmp)==2:
+        print(tmp["name"])
+        Tournament.Insert(tmp["name"],output_json["data"]["title"],output_json["data"]["date_start_text"])
+    else:
+        print(output_json["data"]["tournament"])
+        Tournament.Insert(output_json["data"]["tournament"],output_json["data"]["title"],output_json["data"]["date_start_text"])
+    
     # add team
     print('add team')
     for tmp in output_json["data"]["teams"]:
         Team.Insert(tmp["id"],tmp["name"])
         print(tmp["id"],tmp["name"])
         # Team.Insert(tmp["id"],tmp["name"])
-
-    # add tournamte
-    print('add tournamte')
-    tmp = output_json["data"]["tournament"]
-    if len(tmp)==2:
-        print(tmp["name"])
-        Tournament.Insert(tmp["name"])
-    else:
-        print(output_json["data"]["tournament"])
-        Tournament.Insert(output_json["data"]["tournament"])
 
 channel.basic_consume(queue='eSportQueue', on_message_callback=callback, auto_ack=True)
 
--- backend/consumer/model.py
+++ backend/consumer/model.py
@@ -16,114 +16,129 @@ class Title(db.Model):
     __tableName__ = "title"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    date_start_text = db.Column(db.String(120),nullable=False)
     state = db.Column(db.Integer,nullable=False)
 
     @staticmethod
-    def Insert(_Title,_date_start_text,_state):
+    def Insert(_Title,_state):
         _Title = _Title.strip()
         print(_Title)
         print(Title.query.filter_by(title=_Title).count())
         if Title.query.filter_by(title=_Title).count()==0:
-            new_title = Title(title= _Title,date_start_text=_date_start_text,state=_state)
+            new_title = Title(title= _Title,state=_state)
             try:
                 db.session.add(new_title)
                 db.session.commit()
                 print('add Title')
             except:
                 return 'error'
-        # else:
-        #     Title_info = Title.query.filter_by(title=_Title).first()
-        #     title_id = Title_info.id
-        #     print('title id is '+ str(title_id))
-
-
 
 class Tournament(db.Model):
     __tableName__ = "tournamet"
     id = db.Column(db.Integer, primary_key=True)
     tournament = db.Column(db.String(120), nullable=False)
+    date_start_text = db.Column(db.String(120),nullable=False)
+    # ForeignKey
     title = db.relationship('Title', backref='tournamet', lazy=True)
     title_id = db.Column(db.Integer, db.ForeignKey('title.id'))
 
     @staticmethod
-    def Insert(_Tournament):
-
-
-
-        new_tournaments = Tournament(tournament=_Tournament,title_id=1)
-        # try:
-        db.session.add(new_tournaments)
-        res = db.session.commit()
-        print('add Tournament')
-        #     return 200
-        # except:
-        #     return 500
-
-
-
+    def Insert(_Tournament,_title,_date_start_text):
+        _Tournament=_Tournament.strip()
+        _title=_title.strip()
+        if (Tournament.query.filter_by(Tournament=_Tournament).count==0):
+            Title_id = Title.query.filter_by(title=_title).first().id
+            new_tournaments = Tournament(tournament=_Tournament,title_id=Title_id,date_start_text=_date_start_text)
+            try:
+                db.session.add(new_tournaments)
+                res = db.session.commit()
+                print('add Tournament')
+            except:
+                return 'error'
 
 class Team(db.Model):
     __tableName__ = "team"
     id = db.Column(db.Integer, primary_key=True)
     Team = db.Column(db.String(120), nullable=False)
-    Team_id = db.Column(db.Integer,nullable=False)
+    # Team_id = db.Column(db.Integer,nullable=False)
 
     @staticmethod
     def Insert(_Team_id,_Team):
-        new_team = Team(Team=_Team,Team_id=_Team_id)
-        # try:
-        db.session.add(new_team)
-        db.session.commit()
-        print('add Team')
-        #     return 200
-        # except:
-        #     return 500
+        if (Team.query.filter_by(id=_Team_id).count==0):
+            new_team = Team(id=_Team_id,Team=_Team)
+            try:
+                db.session.add(new_team)
+                db.session.commit()
+                print('add Team')
+            except:
+                return 'error'
 
 class Matches(db.Model):
     __tableName__ = "matches"
     id = db.Column(db.Integer, primary_key=True)
-    Title = db.Column(db.String(120), nullable=False)
-    Tournament = db.Column(db.String(120), nullable=True)
-    Team = db.Column(db.String(120), nullable=True)
-    Match = db.Column(db.String(120), nullable=True)
-    Scores= db.Column(db.Integer, nullable=True)
+     # ForeignKey
+    tournament = db.relationship('Tournament', backref='tournamet', lazy=True)
+    Tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'))
+     # ForeignKey
+    Team1_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    Team2_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+
+    Team1 = db.relationship('Team', foreign_keys='team',backref='team1_id', lazy=True)
+    Team2 = db.relationship('Team', foreign_keys='team',backref='team2_id', lazy=True)
+
+# class Company(Base):
+#     __tablename__ = 'company'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(50), nullable=False)
+
+# class Stakeholder(Base):
+#     __tablename__ = 'stakeholder'
+#     id = Column(Integer, primary_key=True)
+
+#     company_id = Column(Integer, ForeignKey('company.id'), nullable=False)
+#     stakeholder_id = Column(Integer, ForeignKey('company.id'), nullable=False)
+#     company = relationship("Company", foreign_keys=[company_id])
+#     stakeholder = relationship("Company", foreign_keys=[stakeholder_id])
 
 
 
-    def json(self):
-        return {
-                                    'id': self.id, 
-                                    'Title': self.Title, 
-                                    'Tournament': self.Tournament,
-                                    'Team': self.Team,
-                                    'Match': self.Match,
-                                     'Scores': self.Scores
-                                    }
-
-    def __repr__(self):
-        json_matches_table = {
-                                'id': self.id, 
-                                'Title': self.Title, 
-                                'Tournament': self.Tournament,
-                                'Team': self.Team,
-                                'Match': self.Match,
-                                'Scores': self.Scores
-        }
-        return json.dumps(json_matches_table)
 
 
 
-class Scores(db.Model):
-    __tableName__ = "scores"
-    id = db.Column(db.Integer, primary_key=True)
-    Scores = db.Column(db.String(120), nullable=False)
 
-    # def Insert(_Title, _Tournament, _Team,_Match,_Score):
-    #     new_match = Matches(Title= _Title,Tournament=  _Tournament, Team=_Team,Match=_Match,Scores=_Score)
-    #     try:
-    #         db.session.add(new_match)
-    #         db.session.commit()
-    #         return 200
-    #     except:
-    #         return 500
+
+
+
+
+# class Scores(db.Model):
+#     __tableName__ = "scores"
+#     team_id = db.Column(db.Integer, primary_key=True)
+#     winner = db.Column(db.String(120), nullable=False)
+#     Score = db.Column(db.String(120), nullable=False)
+
+
+
+
+    # def json(self):
+    #     return {
+    #                                 'id': self.id, 
+    #                                 'Title': self.Title, 
+    #                                 'Tournament': self.Tournament,
+    #                                 'Team': self.Team,
+    #                                 'Match': self.Match,
+    #                                  'Scores': self.Scores
+    #                                 }
+
+    # def __repr__(self):
+    #     json_matches_table = {
+    #                             'id': self.id, 
+    #                             'Title': self.Title, 
+    #                             'Tournament': self.Tournament,
+    #                             'Team': self.Team,
+    #                             'Match': self.Match,
+    #                             'Scores': self.Scores
+    #     }
+    #     return json.dumps(json_matches_table)
+
+
+
+
